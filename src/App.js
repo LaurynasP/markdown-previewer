b8d279f5ab23b5e17a511923a175e15533231544
Laurynas <laupuskunigis@gmail.com>
2019-03-23 01:47:52 +0100

    Styling and redux prep

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { createStore, dispatch } from 'redux';
 import './App.css';
+/*________________________________Redux______________________________________*/
+
+//Reducers
+function editorReducer(state = {text: "Type to edit!"}, action) {
+  switch (action.type) {
+    case "UPDATE": return {
+      text: action.text
+    };
+    default: return null;
+  }
+}
+
+function previewerReducer(state, action) {
+  switch(action.type) {
+    default: return null;
+  }
+}
+
+//Store
+
+const store = createStore(editorReducer);
+
+//Action creators
+
+const updateText = (newText) => {
+  return {
+    type: "UPDATE",
+    text: newText
+  }
+};
+
+/*________________________________React______________________________________*/
+
+class Editor extends Component {
+  render() {
+    return (
+      <div id = "editorWindow">
+        <div class = "windowTop">
+          <h3 class = "windowTitle" ><i class="fas fa-pencil-alt"></i> Editor</h3>
+        </div>
+        <textarea id = "editor" />
+      </div>
+    );
+  }
+}
+
+class Previewer extends Component {
+  render() {
+    return (
+      <div id = "previewerWindow">
+        <div class = "windowTop">
+          <h3 class = "windowTitle" ><i class="fas fa-file-alt"></i> Previewer</h3>
+        </div>
+        <p>
+        </p>
+      </div>
+    );
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Editor />
+        <Previewer />
       </div>
     );
   }
